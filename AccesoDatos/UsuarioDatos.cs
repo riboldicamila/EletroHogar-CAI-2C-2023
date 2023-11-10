725dc6a8d872ec0dec5f6a5dbd6346ce089e5c8c
Camila Riboldi <cami.riboldi@gmail.com>
2023-11-10 18:45:09 -0300

    métodos de http en usuario basado en ejemplo ws

--- AccesoDatos/UsuarioDatos.cs
+++ AccesoDatos/UsuarioDatos.cs
@@ -10,6 +10,41 @@ namespace AccesoDatos
     {
         static String usuarioAdmin = "70b37dc1-8fde-4840-be47-9ababd0ee7e5";
 
+
+        //POST para login
+        public static string Login(Login login)
+        {
+            var jsonRequest = JsonConvert.SerializeObject(login);
+
+            HttpResponseMessage response = WebHelper.Post("Usuario/Login", jsonRequest);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Verifique los datos ingresados");
+            }
+
+            var reader = new StreamReader(response.Content.ReadAsStream());
+
+            String respuesta = reader.ReadToEnd();
+
+            return respuesta;
+        }
+
+
+        //POST para agregar usuario
+
+        public static void AgregarUsuario(UsuarioWS usuario)
+        {
+            var jsonRequest = JsonConvert.SerializeObject(usuario);
+            HttpResponseMessage response = WebHelper.Post("Usuario/AgregarUsuario", jsonRequest);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Verifique los datos ingresados. Error al agregar usuario.");
+            }
+        }
+
+
         //GET para traer usuarios
         public static List<UsuarioWS> ListarUsuarios()
         {
@@ -29,28 +64,52 @@ namespace AccesoDatos
             }
         }
 
-        //POST para agregar usuario
-
-        public static void AgregarUsuario(UsuarioWS usuario)
-        {
-            var jsonRequest = JsonConvert.SerializeObject(usuario);
-            HttpResponseMessage response = WebHelper.Post("Usuario/AgregarUsuario", jsonRequest);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception("Verifique los datos ingresados. Error al agregar usuario.");
-            }
-        }
-
 
         //PATCH cambiar contraseña
 
+        public static string CambiarContraseña(string nombreUsuario, string contraseña, string contraseñaNueva)
+        {
+            Dictionary<String, String> map = new Dictionary<String, String>();
+            map.Add("nombreUsuario", nombreUsuario);
+            map.Add("contraseña", contraseña);
+            map.Add("contraseñaNueva", contraseñaNueva);
 
+            var jsonRequest = JsonConvert.SerializeObject(map);
 
+            HttpResponseMessage response = WebHelper.Patch("Usuario/CambiarContraseña", jsonRequest);
 
-        //POST para login
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Verifique los datos ingresados");
+            }
+
+            var reader = new StreamReader(response.Content.ReadAsStream());
+
+            String respuesta = reader.ReadToEnd();
+
+            return respuesta;
+        }
+
 
         //DELETE para baja de usuario
+
+        public static void BorrarUsuario(string idUsuario, string idUsuarioMaster)
+        {
+            Dictionary<String, String> map = new Dictionary<String, String>();
+            map.Add("id", idUsuario);
+            map.Add("idUsuario", idUsuarioMaster);
+
+            var jsonRequest = JsonConvert.SerializeObject(map);
+
+            HttpResponseMessage response = WebHelper.DeleteConBody("Usuario/BajaUsuario", jsonRequest);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Verifique los datos ingresados");
+            }
+
+        }
+
 
         //PATCH para reactivar usuario
 
