b7a831e4259c516f83f643bef89a993b91819c47
juanmarimmele <juanmartin.rimmele@gmail.com>
2023-11-19 14:53:53 -0300

    Formularios - login funcionando Menu admin, sup y vendedor falta adaptar

--- Formulario/FormLogin.cs
+++ Formulario/FormLogin.cs
@@ -30,7 +30,10 @@ namespace Formulario
         private void btnLogin_Click(object sender, EventArgs e)
         {
             idUsuario = LoginMenu(gestorUsuarios, txtUsername.Text, txtPassword.Text);
+            
 
+
+           
         }
 
         private static string LoginMenu(GestorDeUsuarios gestorDeUsuarios, string nombreUsuario, string contraseña)
@@ -43,22 +46,24 @@ namespace Formulario
             try
             {
                 string idUsuario = gestorDeUsuarios.Login(login);
-                Console.WriteLine("Login exitoso. El idUusario es " + idUsuario);
+                MessageBox.Show("Login exitoso. El idUusario es " + idUsuario);
                 gestorDeUsuarios.LimpiarListaDeControl(nombreUsuario);
                 return idUsuario;
 
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Error al hacer login.");
-                Console.WriteLine(ex.Message);
+                MessageBox.Show("Error al hacer login.");
+                MessageBox.Show(ex.Message);
 
                 bool quedanIntentos = gestorDeUsuarios.ListaDeControl(nombreUsuario);
 
                 if (!quedanIntentos)
                 {
-                    Console.WriteLine("El usuario está bloqueado. Demasiados intentos fallidos.");
+                    MessageBox.Show("El usuario está bloqueado. Demasiados intentos fallidos.");
+                    MessageBox.Show("La aplicación se Cerrará");
                     return "error";
+                    Application.Exit();
                 }
             }
 
@@ -66,5 +71,201 @@ namespace Formulario
 
 
         }
+
+        private void MostrarMenu(string idUsuarioActual, string nombreUsuario, string password)
+        {
+            //con idUsuario, get de usuarios con ws
+            //buscar usuario en la lista, ver que host id tiene
+            //define el perfil para hacer al menu
+            //metodo me devuelva un string con tipo de usuario
+
+            idUsuarioActual = idUsuarioActual.Trim('"');
+
+            if (password == "Temp1234")
+            {
+                SolicitarCambioDeContraseña(nombreUsuario, password);
+
+            }
+
+
+            string usuarioActualTipo = gestorUsuarios.TipoDeUsuarioLogin(idUsuarioActual);
+
+            MessageBox.Show(usuarioActualTipo);
+
+            Console.Clear();
+
+            
+            if (usuarioActualTipo == "vendedor")
+            {
+                new MenuVendedor();
+            }
+            else if (usuarioActualTipo == "supervisor")
+            {
+                new MenuSupervisor();
+            }
+            else new MenuAdministrador();
+
+
+            //Modificar segun acciones de botón
+            // Se haria en el codigo de cada formulario específico
+            if (usuarioActualTipo == "administrador")
+            {
+                //Alta Usuarios Supervisores
+                if (opcionSeleccionada == "1")
+                {
+                    Console.WriteLine("GENERAR ALTA/NUEVO USUARIO SUPERVISOR");
+                    RegistrarUsuarioSupervisor(idUsuarioActual);
+
+                }
+
+                //Modificacion de Usuarios Supervisores
+                if (opcionSeleccionada == "2")
+                {
+                    ReactivarUsuario(idUsuarioActual);
+
+                }
+
+                //Baja Supervisores
+                if (opcionSeleccionada == "3")
+                {
+                    BajaUsuarios(idUsuarioActual, "SUPERVISOR");
+                }
+
+                //ALTA Vendedores
+                if (opcionSeleccionada == "4")
+                {
+
+                    Console.WriteLine("GENERAR ALTA/NUEVO USUARIO VENDEDORES");
+                    RegistrarUsuarioVendedor(idUsuarioActual);
+
+                }
+
+                if (opcionSeleccionada == "5")
+                {
+                    ReactivarUsuario(idUsuarioActual);
+
+                }
+
+                if (opcionSeleccionada == "6")
+                {
+                    //VENDEDOR
+                    BajaUsuarios(idUsuarioActual, "VENDEDOR");
+
+                }
+
+                if (opcionSeleccionada == "7")
+                {
+                    AltaProveedores(idUsuarioActual);
+                }
+
+                if (opcionSeleccionada == "8")
+                {
+                    //NO ESTA IMPLEMENTADO
+                    ModificacionProveedores();
+                }
+
+                if (opcionSeleccionada == "9")
+                {
+
+                    BajaProveedores(idUsuarioActual);
+                }
+
+                if (opcionSeleccionada == "10")
+                {
+                    AltaProducto(idUsuarioActual);
+                }
+
+                if (opcionSeleccionada == "11")
+                {
+                    ModificarProducto();
+                }
+
+                if (opcionSeleccionada == "12")
+                {
+                    BajaProducto(idUsuarioActual);
+                }
+            }
+
+
+            if (usuarioActualTipo == "supervisor")
+            {
+                if (opcionSeleccionada == "1")
+                {
+                    AltaProducto(idUsuarioActual);
+                }
+
+                if (opcionSeleccionada == "2")
+                {
+                    ModificarProducto();
+                }
+
+                if (opcionSeleccionada == "3")
+                {
+                    BajaProducto(idUsuarioActual);
+                }
+
+                if (opcionSeleccionada == "4")
+                {
+                    DevolverVentas(idUsuarioActual);
+                }
+            }
+
+
+            if (usuarioActualTipo == "vendedor")
+            {
+                if (opcionSeleccionada == "1")
+                {
+                    RegistrarVenta(idUsuarioActual);
+                }
+
+                if (opcionSeleccionada == "2")
+                {
+                    ReporteVentas();
+                }
+
+                if (opcionSeleccionada == "3")
+                {
+                    RegistrarCliente(idUsuarioActual);
+                }
+
+                if (opcionSeleccionada == "4")
+                {
+                    ModificarCliente(idUsuarioActual);
+                }
+
+            }
+
+
+            Console.WriteLine("Cerrando sesión...");
+            Thread.Sleep(8000);
+            Console.Clear();
+            Iniciar();
+
+        }
+        
+        //Adaptar a formulario
+        private void SolicitarCambioDeContraseña(string nombreUsuario, string contraseña)
+        {
+           
+            MessageBox.Show("Debe cambiar su contraseña.");
+            bool cambioExitoso = false;
+            string nuevaContraseña;
+
+            while (!cambioExitoso)
+            {
+                MessageBox.Show("Ingrese la nueva contraseña:");
+                nuevaContraseña = Console.ReadLine();
+
+                try
+                {
+                    cambioExitoso = gestorUsuarios.EstablecerContraseña(nombreUsuario, contraseña, nuevaContraseña);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
     }
+    
 }
