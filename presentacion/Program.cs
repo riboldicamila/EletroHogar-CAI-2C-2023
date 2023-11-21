162f709b612a24fc988930a89d986befebc154cc
Camila Riboldi <cami.riboldi@gmail.com>
2023-11-21 16:42:56 -0300

    reactivar usuario

--- presentacion/Program.cs
+++ presentacion/Program.cs
@@ -937,10 +937,11 @@ namespace MyApp
 
         private void ReactivarUsuario(string idUsuarioActual)
         {
+            ListarTodosLosUsuarios();
             Console.WriteLine("Ingrese el id del usuario que desea reactivar:");
             string idReactivar = Console.ReadLine();
 
-            bool reactivadoExitoso = gestorUsuarios.BajaUsuarios(idReactivar, idUsuarioActual);
+            bool reactivadoExitoso = gestorUsuarios.ReactivarUsuario(idReactivar, idUsuarioActual);
 
             if (reactivadoExitoso)
             {
